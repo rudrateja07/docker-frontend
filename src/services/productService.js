9c451748fcbcd91dcdba3c97c35f5aefa56f925c
rudrateja07 <rudrateja.chandu07@gmail.com>
2025-09-20 09:49:41 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -15,5 +15,5 @@ export const getProducts = async (category = "") => {
 
 // Get product image URL
 export const getProductImageUrl = (imagePath) => {
-  return `http://ec2-13-48-192-1.eu-north-1.compute.amazonaws.com:8081/api/products/images/${imagePath}`;
+  return `http://ec2-16-170-226-233.eu-north-1.compute.amazonaws.com:8081/api/products/images/${imagePath}`;
 };
